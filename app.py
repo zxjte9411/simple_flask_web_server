cceeab5c8cdc8f16084231206600195ac16dff33
zxjte9411 <a5852241@gmail.com>
2018-10-01 13:25:43 +0800

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+def main():
+    app.run('0.0.0.0')
+
+
+if __name__ == '__main__':
+    main()
